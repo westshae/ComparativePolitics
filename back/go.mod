27b2ed6d54b147957e1abd7c167e014f07fda8f9
Shae West <shaewest02@gmail.com>
2024-10-27 19:57:50 +1300

    Fixed neo4j too short default password

--- back/go.mod
+++ back/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/neo4j/neo4j-go-driver/v4 v4.4.7 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.56.0 // indirect
--- back/main.go
+++ back/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
 // User struct represents a simple data model
@@ -11,7 +15,22 @@ type User struct {
 	Age  int    `json:"age"`
 }
 
+var driver neo4j.Driver
+
+func initNeo4j() {
+	var err error
+	// Connect to the Neo4j database
+	driver, err = neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("neo4j", "password123", ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	// Initialize Neo4j connection
+	initNeo4j()
+	defer driver.Close()
+
 	// Create a new Fiber app
 	app := fiber.New()
 
@@ -38,12 +57,25 @@ func main() {
 			return c.Status(400).JSON(fiber.Map{"error": "cannot parse JSON"})
 		}
 
-		// You can process the user data here (e.g., save to a database)
+		// Save the user data to Neo4j
+		session := driver.NewSession(neo4j.SessionConfig{DatabaseName: "neo4j"})
+		defer session.Close()
+
+		_, err := session.Run("CREATE (u:User {id: $id, name: $name, age: $age})", map[string]interface{}{
+			"id":   user.ID,
+			"name": user.Name,
+			"age":  user.Age,
+		})
+		if err != nil {
+			fmt.Println("Error could not create user:", err) // Print error to console
+
+			return c.Status(500).JSON(fiber.Map{"error": "could not create user"})
+		}
 
 		// Respond with the created user data
 		return c.Status(201).JSON(user)
 	})
 
-	// Start server on port 3000
+	// Start server on port 5000
 	app.Listen(":5000")
 }
