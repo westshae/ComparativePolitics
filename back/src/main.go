29aaced1b342d86aa5d85e74723678d97ee33586
Shae West <shaewest02@gmail.com>
2024-10-29 16:25:47 +1300

    Annoying .env prod shit

--- back/src/main.go
+++ back/src/main.go
@@ -40,7 +40,7 @@ func main() {
 }
 
 func initNeo4j() (neo4j.Driver, error) {
-	err := godotenv.Load("../")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
